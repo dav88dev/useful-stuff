31d9b080ddb905621cdbb8198974085fe983ec50
David Aghayan <admin@dav88dev.com>
2020-10-01 22:11:17 +0400

    Update speedbump.js
    improvements

--- speedbump.js
+++ speedbump.js
@@ -1,7 +1,7 @@
 //Insert Bootstrap modal into body and speedbump on jQuery
 
     $(document).ready(function(){
-        document.body.innerHTML += '<div class="modal fade" tabindex="-1" role="dialog" id="speedbump"><div class="modal-dialog"><div class="modal-content"> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title">Warning</h4></div><div class="modal-body"><p>You are leaving !</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button><button type="button" onclick="openLink()" class="btn btn-danger">OK</button></div></div></div></div>';
+        $('.someClass').append('<div class="modal fade" tabindex="-1" role="dialog" id="speedbump"><div class="modal-dialog"><div class="modal-content"> <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h4 class="modal-title" style="color: black">Warning</h4></div><div class="modal-body"><p style="color: black">You are leaving !</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button><button type="button" onclick="openLink()" class="btn btn-danger">OK</button></div></div></div></div>');
         // external link speedbump
         $.expr[":"].external = function (a) {
             // DO NOT INCLUDE THE FOLLOWING MATCHES: EMAIL LINKS, TELEPHONE LINKS, HOSTNAME
